0359d293ec9a8cbbfe91779a32f897f89a28e31a
tpcstld <jerryjiang1128@gmail.com>
2013-09-18 23:27:36 -0400

    Created GameOptionsFragment

---
+++ src/com/tpcstld/jetris/GameOptionsFragment.java
@@ -0,0 +1,15 @@
+package com.tpcstld.jetris;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+@SuppressLint("NewApi")
+public class GameOptionsFragment extends PreferenceFragment {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+
+}
